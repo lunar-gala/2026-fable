1000280c2993734f8b3e25e76c0c59ec87e91760
Lily Qin <lilyqin527@gmail.com>
2026-01-29 19:01:53 -0500

    initial commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export', basePath: process.env.NODE_ENV === 'production' ? '/lunar-gala-test' : '',
 };
 
 export default nextConfig;
