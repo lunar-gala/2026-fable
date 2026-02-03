bb0f153dd7fda956421af55d9921cdc316df5880
serenacaii <118383524+serenacaii@users.noreply.github.com>
2026-02-03 17:21:55 -0500

    fixed merge conflict?

--- src/app/components/MainContent.tsx
+++ src/app/components/MainContent.tsx
@@ -3,16 +3,10 @@
 import { Content } from "next/dist/compiled/@next/font/dist/google";
 import { COLORS } from "../constants";
 import LandingAnimation from "./Landing/LandingAnimation";
-<<<<<<< Updated upstream
-import { motion, useScroll } from "motion/react"
-=======
-import LandingAssetAnimation from "./Landing/LandingAssetAnimation";
 import TeaserGradientAnimation from "./Teaser/TeaserGradientAnimation";
-import WordmarkAnimation from "./Landing/WordmarkAnimation";
 import { motion, useScroll } from "motion/react";
-import CountdownTeaser from "./Teaser/CountdownTeaser";
 
->>>>>>> Stashed changes
+
 export default function MainContent() {
 
   return (
@@ -28,12 +22,8 @@ export default function MainContent() {
         transition: "opacity 0.1s ease-out",
       }}
       >
-<<<<<<< Updated upstream
       <LandingAnimation />
 
-=======
-      <TeaserGradientAnimation />
->>>>>>> Stashed changes
       </div>
     </>
 
