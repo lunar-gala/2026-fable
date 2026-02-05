6b2d97b726a01d10327149bfc14fcba92d070eb8
serenacaii <serenacai502@gmail.com>
2026-02-05 03:54:14 -0500

    added teaser page text & countdown, readjusted gradient animation

--- src/app/components/MainContent.tsx
+++ src/app/components/MainContent.tsx
@@ -4,6 +4,7 @@ import { Content } from "next/dist/compiled/@next/font/dist/google";
 import { COLORS } from "../constants";
 import LandingAnimation from "./Landing/LandingAnimation";
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 import TeaserGradientAnimation from "./Teaser/TeaserGradientAnimation";
 import { motion, useScroll } from "motion/react";
 =======
@@ -15,6 +16,14 @@ import Countdown from "./Teaser/Countdown";
 import WordmarkAnimation from "./Landing/WordmarkAnimation";
 >>>>>>> Stashed changes
 
+=======
+import LandingAssetAnimation from "./Landing/LandingAssetAnimation";
+import TeaserGradientAnimation2 from "./Teaser/TeaserGradientAnimation2";
+import TeaserContent from "./Teaser/TeaserContent";
+import TeaserGradientAnimation from "./Teaser/TeaserGradientAnimation";
+import Countdown from "./Teaser/Countdown";
+import WordmarkAnimation from "./Landing/WordmarkAnimation";
+>>>>>>> Stashed changes
 
 export default function MainContent() {
 
@@ -32,12 +41,18 @@ export default function MainContent() {
       }}
       >
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
       <TeaserGradientAnimation />
 =======
+=======
+>>>>>>> Stashed changes
       <div className="gradient-grid teaser-textcontent">      
         <TeaserContent/>
       </div>
       <TeaserGradientAnimation2/>
+<<<<<<< Updated upstream
+>>>>>>> Stashed changes
+=======
 >>>>>>> Stashed changes
 
       </div>
