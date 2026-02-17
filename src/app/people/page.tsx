1d6bd6e3315b24db1200475fa225f407dbdf9649
tu2463 <117417955+tu2463@users.noreply.github.com>
2026-02-17 17:29:36 -0500

    route to people

---
+++ src/app/people/page.tsx
@@ -0,0 +1,11 @@
+import NavBar from "../components/NavBar";
+import PeopleContent from "../components/People/PeopleContent";
+
+export default function AboutPage() {
+  return (
+    <div className="pageContent">
+      <NavBar />
+      <PeopleContent />
+    </div>
+  );
+}
